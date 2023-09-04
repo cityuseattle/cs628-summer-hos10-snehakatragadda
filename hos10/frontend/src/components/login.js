2c4f190d10e4f09f55fda006c957a912ee8edaef
snehakatragadda <103678318+snehakatragadda@users.noreply.github.com>
2023-09-04 22:55:24 +0000

    Submission of Module10-Snehakatragadda

---
+++ hos10/frontend/src/components/login.js
@@ -0,0 +1,15 @@
+
+
+//HOS10A login.js
+import React from "react";
+
+const Login = () => {
+  return (
+    <a href="https://special-engine-7j6vr4qv6vjcxg9g-5050.app.github.dev/facebook" 
+    className="btn btn-primary">
+      Login with Facebook
+    </a>
+  );
+};
+
+export default Login;
